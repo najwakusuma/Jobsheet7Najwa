c1b4129423851f5be44db56bbd06839b0c9aa077
user <user@DESKTOP-JBIISF7>
2018-08-01 09:26:00 +0700

    initial commit

---
+++ src/Latihan/Latihan1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan;
+
+/**
+ *
+ * @author user
+ */
+public class Latihan1 {
+    public static void main(String[] args) {
+        String identitas = "Najwa Kusuma Mustafa / XR5 / 28";
+        System.out.println("identitas : "+ identitas);
+        
+}
