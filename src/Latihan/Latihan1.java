9da2a201347bb27d6f5930caf504d844c5838a17
user <user@DESKTOP-JBIISF7>
2018-08-03 11:09:41 +0700

    coba IF

--- src/Latihan/Latihan1.java
+++ src/Latihan/Latihan1.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package Latihan;
-
+import java.util.Scanner;
 /**
  *
  * @author user
@@ -14,5 +14,20 @@ public class Latihan1 {
         String identitas = "Najwa Kusuma Mustafa / XR5 / 28";
         System.out.println("identitas : "+ identitas);
         
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nilai a ?");
+        int a = scanner.nextInt();
         
+        System.out.println("\nIF.. 1");
+        if (a < 5) System.out.println("nilai a kurang dari 5");
+        
+        System.out.println("\nIF.. 2");
+        if (a == 5)
+             System.out.println("nilai a sama dengan 5");
+        
+        System.out.println("\nIF.. 3");
+        if (a > 5);
+             System.out.println("isi variabel a : "+ a);
+             System.out.println("nilai a lebih dari 5");
+        }
 }
