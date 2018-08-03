2cd7ab2068e92b50e6d32fe6db5ee9f16be025b7
user <user@DESKTOP-JBIISF7>
2018-08-03 19:27:30 +0700

    InitialCommit

---
+++ src/Latihan/SaranBahan2Resep.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan;
+
+/**
+ *
+ * @author user
+ */
+public class SaranBahan2Resep {
+    public static void main(String[] args) {
+        String identitas = "najwa kusuma mustafa / xr5 / 28";
+        System.out.println("Identiras : "+ identitas);
+        
+        System.out.print("\nSaran Resep dari Bahan milik anda\n");
+    }
+}
