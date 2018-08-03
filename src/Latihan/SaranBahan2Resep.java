4c96b49a9316fcd2ea77e6b5850cf3c636bbb664
user <user@DESKTOP-JBIISF7>
2018-08-03 19:43:05 +0700

    TambahInputBahan2

--- src/Latihan/SaranBahan2Resep.java
+++ src/Latihan/SaranBahan2Resep.java
@@ -22,5 +22,25 @@ public class SaranBahan2Resep {
         System.out.println("2. Telur");
         System.out.print("Masukkan no pilihan Anda :");
         int bahan1 = scanner.nextInt();
-    }
-}
+        if(bahan1 == 1 || bahan1 == 2)
+        {
+            System.out.println("Bahan Kedua: ");
+            if(bahan1 == 1){
+                System.out.println("1. Susu");
+                System.out.println("2. Minyak Goreng");
+                System.out.println("3.Tidak ada");
+            }else{
+                System.out.println("1. Minyak Goreng");
+                System.out.println("2. Roti");
+                System.out.println("3. Tidak ada");
+            }
+            System.out.print("masukkan no pilihan anda :");
+            int bahan2 = scanner.nextInt();
+            
+        }else
+            System.out.println("mohon maaf, pilihan tidak ditemukan," 
+                    + "tidak dapat memberikan saran resep");
+            }
+        }
+    
+
